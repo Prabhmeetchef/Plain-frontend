ce4c65f3d5a4476acc0ae77cd8d621511a2d36e3
Prabhmeet Singh <98380208+Prabhmeetchef@users.noreply.github.com>
2025-02-26 04:58:08 +0530

    Portable screens availabilty removed

---
+++ src/app/components/PortableBlocker.tsx
@@ -0,0 +1,25 @@
+"use client";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+export default function MobileBlocker({ children }: { children: React.ReactNode }) {
+  const [isMobile, setIsMobile] = useState<boolean | null>(null);
+  useEffect(() => {
+    const userAgent = navigator.userAgent.toLowerCase();
+    const mobileDevices = /iphone|ipad|ipod|android|blackberry|opera mini|iemobile|mobile/;
+    setIsMobile(mobileDevices.test(userAgent));
+  }, []);
+  if (isMobile === null) return null; // Prevent flickering
+  if (isMobile)
+    return (
+      <div className="h-screen flex flex-col items-center justify-center bg-[#cbcbcb] text-white text-center p-6">
+        <div className="max-w flex bg-gradient-to-b from-purple-600 to-[#290038] p-8 rounded-2xl shadow-lg flex-col items-center justify-center gap-10">
+          <Image src="/Phone.png" alt="phone" width={100} height={100}/>
+          <p className="text-[20px] opacity-80">
+            Sorry, This website is only accessible on a <span className="font-semibold">desktop browser</span>.
+          </p>
+        </div>
+      </div>
+    );
+
+  return <>{children}</>;
+}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
 import type { Metadata } from "next";
+import PortableBlocker from "@/app/components/PortableBlocker";
+import React from "react";
 import { Roboto_Condensed } from "next/font/google";
-import "./globals.css"
-import AuthProvider from '@/app/components/SessionProvider';
+import "./globals.css";
+import AuthProvider from "@/app/components/SessionProvider";
 const robotoCondensed = Roboto_Condensed({
   subsets: ["latin"],
   weight: ["400", "700"],
@@ -16,9 +18,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={robotoCondensed.className}>
-      <AuthProvider>
-          {children}
-        </AuthProvider>
+        <PortableBlocker>
+          <AuthProvider>{children}</AuthProvider>
+        </PortableBlocker>
       </body>
     </html>
   );
